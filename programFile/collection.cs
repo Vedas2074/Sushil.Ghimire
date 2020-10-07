999955b86d283e0fa4cb4322fa7d659dea716b7b
vedghimi2054 <kamalaghimireppoe2032@gmail.com>
2020-10-07 18:46:27 +0545

    Learned about inheritence

--- programFile/collection.cs
+++ programFile/collection.cs
@@ -4,7 +4,7 @@ public class Collections
 {
     public void LearnAboutList()
     {
-        List<string> names= new List<string>(){"Ram","sita"};
+        List<string> names = new List<string>() { "Ram", "sita" };
         names.Add("sushil");
         names.Add("Ragav");
         names.Add("avinas");
@@ -14,14 +14,15 @@ public class Collections
     public void LearnAboutDictonary()
     {
         //holds key-value pairs
-        Dictionary<string,string> countryCapitals = new Dictionary<string,string>();
+        Dictionary<string, string> countryCapitals = new Dictionary<string, string>
         {
-            
+            ["Bhutan"] = "Thimpu",
+            ["China"] = "Beijing"
         };
-        
-        countryCapitals.Add("nepal","kathmandu");
-        countryCapitals.Add("india","Delhi");
-        countryCapitals.Add("usa","wdc");
-        countryCapitals.Add("Russia","Mosco");
+
+        countryCapitals.Add("nepal", "kathmandu");
+        countryCapitals.Add("india", "Delhi");
+        countryCapitals.Add("usa", "wdc");
+        countryCapitals.Add("Russia", "Mosco");
     }
 }
