0c1d498a1432f2c9f2dd1cafa801063d4e1043bb
sushil <ghimires2054@gmail.com>
2020-12-07 18:25:08 +0545

    Learned About EmployeeManagenent models

---
+++ WebAppMvc/Models/Department.cs
@@ -0,0 +1,12 @@
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+
+        public float Price { get; set; }
+        public bool Status { get; set; }
+        public int Quantity { get; set; }
+    }
+}
