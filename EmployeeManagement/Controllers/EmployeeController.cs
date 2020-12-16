775717ddbd7b54aef1bd659e02bc1682cd8720cb
sushil <ghimires2054@gmail.com>
2020-12-16 19:18:17 +0545

    Learned HttpPost and Controllers Model Binding

--- EmployeeManagement/Controllers/EmployeeController.cs
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -11,16 +11,22 @@ namespace EmployeeManagement.Controllers
             var employees = Employee.GetEmployee();
             return View(employees);
         }
-        public ActionResult Detail(int id)
+        public ActionResult Detail([FromQuery] int id)
         {
             var employees = Employee.GetEmployee();
             var emp = employees.Where(x => x.Id == id).FirstOrDefault();
             return View(emp);
 
         }
+        [HttpGet]//default always have [HttpGet] method
         public IActionResult Add()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult<string> Add([FromForm] Employee employee)//controllers model binding
+        {
+            return "save record";
         }
     }
 
